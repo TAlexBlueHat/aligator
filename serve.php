632eb7bfa3cdc185046e8c91b99e196e6df50ecd
Alex TONDOH <alextondoh@gmail.com>
2017-03-07 22:21:56 +0000

    FileName Modify
    Modification du nom Workers => Predator for console Script name

--- serve.php
+++ serve.php
@@ -1,14 +1,12 @@
 <?php
 /**
- * Aligator - A minimilize PHP Framework For Web workers
+ * Aligator - A minimilize PHP Framework For Web Predator
  *
  * @package  Aligator
  * @author   Alex TONDOH <alextondoh@gmail.com>
  */
 
-$uri = urldecode(
-    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
-);
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
