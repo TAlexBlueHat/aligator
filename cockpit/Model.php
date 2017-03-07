ec2d57657fed1f3134f7ed2b9f468f243f0b2979
Alex TONDOH <alextondoh@gmail.com>
2017-03-07 00:16:28 +0000

    Fichier de base

---
+++ cockpit/Model.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Aligator - A minimilize PHP Framework For Web workers
+ *
+ * @package  Aligator
+ * @author   Alex TONDOH <alextondoh@gmail.com>
+ */
+
+namespace Aligator\cockpit;
+
+
+class Model {
+
+}
